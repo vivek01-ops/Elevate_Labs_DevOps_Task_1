f5cddb0abb749b31d4900b18fc683b1ab90261f7
vivek01-ops <chaudharivivek.0124@gmail.com>
2025-04-07 10:59:20 +0530

    commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => res.send('Hello, DevOps!'));
+app.get('/', (req, res) => res.send('Hello, Elevate Labs, this is my fist DevOps task!'));
 app.listen(port, () => console.log(`App listening on port ${port}`));
